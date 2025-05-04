881a28dd62e17211ced7d4dc993ee5ead32f88fe
Adam Rioux <adamlrioux@gmail.com>
2025-05-04 14:33:19 -0400

    - update main.tf to provision ansible master separately and install ansible - update variables.tf to not include ansible server as part of the instance map

--- main.tf
+++ main.tf
@@ -4,6 +4,8 @@ provider "google" {
   region      = "us-east1"
 }
 
+# K8s VMs
+
 resource "google_compute_instance" "vm_instance" {
   for_each     = var.instances
   name         = each.key
@@ -27,7 +29,45 @@ resource "google_compute_instance" "vm_instance" {
 
 }
 
-output "ansible_master_ip" {
-  value = google_compute_instance.vm_instance["ansible-master"].network_interface[0].access_config[0].nat_ip
+# Ansible master
+resource "google_compute_instance" "ansible_master" {
+  name         = "ansible-master"
+  machine_type = "e2-small"
+  zone         = "us-east1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "rocky-linux-cloud/rocky-linux-9"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config {}
+  }
+
+  metadata = {
+    ssh-keys = "${var.username}:${file(var.ssh_public_key)}"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = var.username
+    private_key = file("~/.ssh/gcp_key")
+    host        = self.network_interface[0].access_config[0].nat_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo dnf install -y epel-release",
+      "sudo dnf install -y ansible"
+    ]
+  }
 }
+
+
+output "ansible_master_ip" {
+  value = google_compute_instance.ansible_master.network_interface[0].access_config[0].nat_ip
+}
+
 
